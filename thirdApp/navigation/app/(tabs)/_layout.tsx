c5ce82f169a740400ec3d0e9bf54589712cec12a
Shivam Kumar <bitmshivam@gmail.com>
2025-03-24 05:36:31 +0530

    stack navigation and tab navigator

---
+++ thirdApp/navigation/app/(tabs)/_layout.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
+import { Pressable } from "react-native";
+
+import Colors from "@/constants/Colors";
+import { useColorScheme } from "@/components/useColorScheme";
+import { useClientOnlyValue } from "@/components/useClientOnlyValue";
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>["name"];
+  color: string;
+}) {
+  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+}
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: useClientOnlyValue(false, true),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "RealTime Clock",
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color="red" />,
+          headerRight: () => (
+            <Link href="/modal" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="info-circle"
+                    size={35}
+                    color={Colors[colorScheme ?? "light"].text}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="two"
+        options={{
+          title: "Calculator",
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="calculator" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
